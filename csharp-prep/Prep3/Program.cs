1217c83f8c173ff6ef68af41995d40b54cc2380c
EmanuelMunozTorigino <emanuel.munoz.torigino@gmail.com>
2024-09-22 02:20:17 -0300

    finished all the hw activities, learning about variable and lists declaration and its methods, control and loops structures as well as function in c#

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,71 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.WriteLine("Hello Prep3 World!");
+
+
+        // Guess My Number Game
+
+
+
+        string answer = "yes";
+
+        while (answer == "yes")
+
+        {
+            // Generate a random number from 1 to 100
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 100);
+
+            // 
+            int attempts = 1;
+
+            Console.WriteLine("We are going to play 'Guess The Number' game, I'll think a number and you need to guess it..!");
+            Console.WriteLine();
+
+            Console.WriteLine("Let's start!");
+            Console.WriteLine();
+
+
+
+            Console.WriteLine("What is your guess number? from 1 to 100 ");
+            int guessNumber = int.Parse(Console.ReadLine());
+
+
+            while (guessNumber != magicNumber)
+            {
+                if (guessNumber < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (guessNumber > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                Console.WriteLine("What is your guess number? ");
+                guessNumber = int.Parse(Console.ReadLine());
+
+
+                attempts++;
+
+                // The ++ operator is the same of the folowing operation:
+
+                // attempts = attempts + 1;
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("You guessed it!");
+
+            Console.WriteLine($"Until you guessed the magic number you had {attempts} attempts");
+
+            Console.WriteLine("Type 'yes' to play Again: ");
+            answer = Console.ReadLine();
+
+        }
+        Console.WriteLine("The game is end, thanks to play it!");
+
+
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,67 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        //Console.WriteLine("Hello Prep4 World!");
+
+
+        List<int> numbers = new List<int>();
+
+        int userNumber = -1;
+
+        while (userNumber != 0)
+        {
+            Console.WriteLine("Enter some numbers (number '0' to end): ");
+            userNumber = int.Parse(Console.ReadLine());
+
+            // It functionality verify if the number is a positive number but is not neccesary on this assignment.
+
+            /*while (number < 0)
+            {
+                Console.WriteLine("The number must be a positive number.");
+                Console.WriteLine("Enter positive numbers ('0' to end): ");
+                number = int.Parse(Console.ReadLine());
+            }
+            */
+
+            numbers.Add(userNumber);
+        }
+        int total = 0;
+        int maxNumber = numbers[0];
+        int smallestNumber = 9999999;
+        foreach (int num in numbers)
+        {
+            total = total + num;
+
+            // Look for the max number on the list..
+            if (num > maxNumber)
+            {
+                maxNumber = num;
+            }
+
+            // Look for the smallest number on the list
+            else if (num > 0)
+            {
+                if (num < smallestNumber)
+                    smallestNumber = num;
+            }
+        }
+
+        float average = ((float)total) / numbers.Count;
+
+        Console.WriteLine($"The sum is: {total}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {maxNumber}");
+        Console.WriteLine($"The smallest positive number is: {smallestNumber}");
+        Console.WriteLine($"The sorted list is:");
+
+        // Use the Sort function to ordain the numbers ascendingly
+        numbers.Sort();
+
+        // Go through the list of numbers and display each one on the console
+        foreach (int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+
     }
 }
